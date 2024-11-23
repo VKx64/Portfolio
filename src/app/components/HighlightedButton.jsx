dc387cdd3c8de1aec2c453550495a734a553eef2
VKx64 <kyllevryann@proton.me>
2024-11-23 08:17:47 +0000

    fixed priority issues with the image

--- src/app/components/HighlightedButton.jsx
+++ src/app/components/HighlightedButton.jsx
@@ -4,7 +4,7 @@ import { SparkleFillIcon } from '@primer/octicons-react';
 const HighlightedButton = ({ textValue }) => {
     return (
         <button
-        className="rounded-lg flex items-center gap-2 px-3 py-2 bg-greenButtonBG box-border border-greenButtonBorder border-[2px] text-greenButtonText transition pointer-events-none ">
+        className="max-h-10 rounded-lg flex items-center gap-2 px-3 py-2 bg-greenButtonBG box-border border-greenButtonBorder border-[2px] text-greenButtonText transition pointer-events-none ">
             {/* Icon */}
             <SparkleFillIcon className="w-5 h-5 text-greenButtonText pointer-events-auto" />
             {/* Text */}
